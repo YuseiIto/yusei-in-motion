50840ab74883003b86a56368dbf3694babc3b68c
Yusei Ito <me@yuseiito.com>
2024-02-04 22:27:14 +0900

    feat: 見た目とソースを整える

--- main.js
+++ main.js
@@ -1,54 +1,10 @@
 let audioContext = null;
 let analyser = null;
 let dataArray = null;
+let stream = null;
+const THRESHOLD = 10000; /* 秘伝のタレ. いい感じに調整できるようにしたい */
 
-const run = () => {
-  navigator.mediaDevices
-    .getUserMedia({ video: false, audio: true })
-    .then((stream) => {
-      audioContext = new AudioContext();
-      analyser = audioContext.createAnalyser();
-      analyser.fftSize = 2048;
-      dataArray = new Uint8Array(analyser.fftSize);
-      console.log("Connected!");
-
-      const track = audioContext.createMediaStreamSource(stream);
-      track.connect(analyser);
-    })
-    .catch((err) => {
-      if (err.name === "NotAllowedError") {
-        console.log("You have denied access to your microphone.");
-      } else if (err.name === "NotFoundError") {
-        console.log("No microphone found.");
-      }
-    });
-};
-
-const updateGain = (e) => {
-  gain = e.target.value / 100;
-  console.log("Gain updated to: ", gain);
-};
-
-let voice = false;
-const update = () => {
-  if (analyser == null || dataArray == null) {
-    console.log("Analyser not found!");
-    return;
-  }
-  analyser.getByteFrequencyData(dataArray);
-  const sum = dataArray.reduce((a, b) => a + b, 0);
-
-  if (sum > 10000) {
-    voice = true;
-  } else {
-    voice = false;
-  }
-};
-
-setInterval(update, 100);
-
-/* Face operations */
-
+/* Face patterns */
 const eyesOpened = new Image();
 eyesOpened.src = "./face/eyes_opened.png";
 const eyesClosed = new Image();
@@ -60,16 +16,81 @@ mouthClosed.src = "./face/mouth_closed.png";
 const nose = new Image();
 nose.src = "./face/nose.png";
 
+const startMouth = () => {
+  audioContext = new AudioContext();
+  analyser = audioContext.createAnalyser();
+  const stopButton = document.getElementById("stop-button");
+  stopButton.style.display = "block";
+  const startButton = document.getElementById("start-button");
+  startButton.style.display = "none";
+
+  navigator.mediaDevices
+    .getUserMedia({ video: false, audio: true })
+    .then((s) => {
+      stream = s;
+      const track = audioContext.createMediaStreamSource(stream);
+      track.connect(analyser);
+    })
+    .catch((err) => {
+      if (err.name === "NotAllowedError") {
+        console.log("You have denied access to your microphone.");
+      } else if (err.name === "NotFoundError") {
+        console.log("No microphone found.");
+      }
+    });
+
+  analyser.fftSize = 2048;
+  dataArray = new Uint8Array(analyser.fftSize);
+};
+
+const stopMouth = () => {
+  stream.getTracks().forEach(function (track) {
+    track.stop();
+  });
+
+  audioContext.close();
+  audioContext = null;
+  analyser = null;
+  dataArray = null;
+
+  const stopButton = document.getElementById("stop-button");
+  stopButton.style.display = "none";
+  const startButton = document.getElementById("start-button");
+  startButton.style.display = "block";
+};
+
+const isVoiceDetected = () => {
+  if (analyser == null || dataArray == null) {
+    return false;
+  }
+
+  analyser.getByteFrequencyData(dataArray);
+
+  // FIXME: 雑に合計値をとっているので、ちゃんと声の帯域だけ取るなどしたい
+  const sum = dataArray.reduce((a, b) => a + b, 0);
+
+  return sum > 10000;
+};
+
 let openEyes = true;
+/* タイマーまばたき */
+const toggleEyes = () => {
+  const time = openEyes ? 500 : 10000 * Math.random();
+  openEyes = !openEyes;
+  setTimeout(toggleEyes, time);
+};
 
 const drawFace = () => {
   requestAnimationFrame(drawFace);
+
+  const voice = isVoiceDetected();
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
   const drawItem = (image) => {
     ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
   };
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   if (openEyes) {
     drawItem(eyesOpened);
@@ -87,15 +108,4 @@ const drawFace = () => {
 window.onload = () => {
   requestAnimationFrame(drawFace);
   toggleEyes();
-};
-
-const toggleEyes = () => {
-  if (openEyes) {
-    openEyes = false;
-    setTimeout(toggleEyes, 1000);
-  }else{
-    openEyes = true;
-    setTimeout(toggleEyes, 3000);
-  }
-
 };
